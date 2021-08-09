8d249b321989a48093947463de08f3b9bea888c4
Farras Timorremboko <Farras>
2021-08-09 21:00:08 +0700

    push ec2 trial

---
+++ program/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	port := os.Getenv("HTTP_PORT")
+	if port == "" {
+		port = ":80"
+	}
+	e := echo.New()
+	e.GET("/", hello)
+	e.GET("/:name", helloName)
+	if err := e.Start(port); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func hello(c echo.Context) error {
+	return c.String(200, "Hello Sekai")
+}
+
+func helloName(c echo.Context) error {
+	name := c.Param("name")
+	return c.String(200, fmt.Sprintf("Hello %s", name))
+}
