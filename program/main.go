22034e47874a1d6e5265703ba75c9f9acc61ca42
Farras Timorremboko <Farras>
2021-08-10 16:26:16 +0700

    add database

--- program/main.go
+++ program/main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/labstack/echo"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
+type User struct {
+	gorm.Model
+	Name string
+}
+
 func main() {
+	connectionString := os.Getenv("CONNECTION_STRING")
+	DB, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	DB.AutoMigrate(&User{})
 	port := os.Getenv("HTTP_PORT")
 	if port == "" {
 		port = ":80"
@@ -15,6 +31,7 @@ func main() {
 	e := echo.New()
 	e.GET("/", hello)
 	e.GET("/:name", helloName)
+	e.GET("/user", getUser)
 	if err := e.Start(port); err != nil {
 		fmt.Println(err)
 	}
@@ -27,4 +44,10 @@ func hello(c echo.Context) error {
 func helloName(c echo.Context) error {
 	name := c.Param("name")
 	return c.String(200, fmt.Sprintf("Hello %s", name))
+}
+
+func getUser(c echo.Context) error {
+	var users []User
+	DB.Find(&users)
+	return c.JSON(http.StatusOK, users)
 }
