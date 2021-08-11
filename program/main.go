4dc4a98cdd07a17c93bb41c02a1375629564dd4f
Farras Timorremboko <Farras>
2021-08-11 20:08:40 +0700

    add some fitures for task

--- program/main.go
+++ program/main.go
@@ -31,24 +31,40 @@ func main() {
 	}
 	e := echo.New()
 	e.GET("/", hello)
-	e.GET("/user", getUser)
+	e.GET("/:user", getOneUser)
+	e.GET("/allUser", getUser)
+	e.POST("/post", postName)
 	if err := e.Start(port); err != nil {
 		fmt.Println(err)
 	}
 }
 
 func hello(c echo.Context) error {
-	return c.String(200, "hello")
+	return c.String(200, "hello gais!!")
 }
 
-func helloName(c echo.Context) error {
-	name := c.Param("name")
-	return c.String(200, fmt.Sprintf("Hello %s", name))
+func postName(c echo.Context) error {
+	name := User{}
+	c.Bind(&name)
+	if err := DB.Save(&name).Error; err != nil {
+		return c.String(500, err.Error())
+	}
+	return c.String(200, fmt.Sprintf("Hello %v", name))
 }
 
 func getUser(c echo.Context) error {
 	var users []User = []User{}
 	if err := DB.Find(&users).Error; err != nil {
+		fmt.Println(err)
+		return c.String(500, err.Error())
+	}
+	return c.JSON(http.StatusOK, users)
+}
+
+func getOneUser(c echo.Context) error {
+	userName := c.Param("user")
+	var users User
+	if err := DB.Find(&users, "Name=?", userName).Error; err != nil {
 		fmt.Println(err)
 		return c.String(500, err.Error())
 	}
